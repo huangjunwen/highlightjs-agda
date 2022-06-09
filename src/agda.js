d4fa4679417bdb14a72d246122f565f6d139dc0e
jayven <kassarar@gmail.com>
2022-06-09 21:14:00 +0800

    Add patterns

--- src/agda.js
+++ src/agda.js
@@ -4,12 +4,94 @@ Author: Huang Jun Wen<kassarar@gmail.com>
 Description: Agda syntax
 */
 
+// Some of the patterns are from Prismjs !!
+
 module.exports = function (hljs) {
   return {
     name: 'Agda',
     aliases: ['agda'],
+    keywords: {
+      keyword: [
+        'Set',
+        'abstract',
+        'constructor',
+        'data',
+        'eta-equality',
+        'field',
+        'forall',
+        'hiding',
+        'import',
+        'in',
+        'inductive',
+        'infix',
+        'infixl',
+        'infixr',
+        'instance',
+        'let',
+        'macro',
+        'module',
+        'mutual',
+        'no-eta-equality',
+        'open',
+        'overlap',
+        'pattern',
+        'postulate',
+        'primitive',
+        'private',
+        'public',
+        'quote',
+        'quoteContext',
+        'quoteGoal',
+        'quoteTerm',
+        'record',
+        'renaming',
+        'rewrite',
+        'syntax',
+        'tactic',
+        'unquote',
+        'unquoteDecl',
+        'unquoteDef',
+        'using',
+        'variable',
+        'where',
+        'with'
+      ]
+    },
     contains: [
-      // TODO
+      {
+        className: 'comment',
+        begin: '--',
+        end: '\n'
+      },
+      {
+        className: 'comment',
+        begin: '{-',
+        end: '-}'
+      },
+      {
+        className: 'string',
+        begin: /"(?:\\(?:\r\n|[\s\S])|[^\\\r\n"])*"/
+      },
+      {
+        className: 'title', // xxx :
+        begin: /(?<=^\s*)(\S+)(?=\s*:)/
+      },
+      {
+        className: 'name', // data xxx :
+        begin: /(?<=^(data|record)\s+)\S+/
+      },
+      {
+        className: 'operator',
+        begin: /(?<=^\s*|\s)(?:[=|:∀→λ\\?_]|->)(?=\s)/
+      },
+      {
+        className: 'operator',
+        begin: /(?<=^\s*|\s)λ(?=\{)/
+      },
+      {
+        className: 'punctuation',
+        begin: /[(){}⦃⦄.;@]/
+      }
     ]
   }
 }
